55a0261bc30b3e86ee44393417b5ca46eab2aef8
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-12-28 20:45:18 +0545

    Modified GetComponent to use unordered_map. | Impl HasComponent.

--- core/include/Entity.h
+++ core/include/Entity.h
@@ -1,10 +1,10 @@
 #pragma once
-#include<memory>
-#include<type_traits>
-#include<typeindex>
-#include<unordered_map>
+#include <memory>
+#include <type_traits>
+#include <typeindex>
+#include <unordered_map>
 
-#include"Core.h"
+#include "Core.h"
 
 namespace Sirius {
 	class Entity {
@@ -14,28 +14,30 @@ namespace Sirius {
 	  public:
 		Entity();
 
-		template<typename T>
-		void AddComponent() {
-			static_assert(std::is_base_of<Component, T>::value, "Type T must inherit from base class of Component!");
-			
-			std::type_index type_idx(typeid(T));
-			
-			if(components.find(type_idx) == components.end()) {
-				components[type_idx] = std::make_unique<T>();
-			}
-		}
-		
-		template<typename T>
-		T* GetComponent() {
+		template <typename T> void AddComponent() {
 			static_assert(std::is_base_of<Component, T>::value, "Type T must inherit from base class of Component!");
 
-			for(auto& component : components) {
-				if(auto ptr = dynamic_cast<T*>(component.get())) {
-					return ptr;
-				}
+			std::type_index typeIndex(typeid(T));
+
+			if (components.find(typeIndex) == components.end()) {
+				components[typeIndex] = std::make_unique<T>();
+			}
+		}
+
+		template <typename T> T* GetComponent() {
+			static_assert(std::is_base_of<Component, T>::value, "Type T must inherit from base class of Component!");
+
+			std::type_index typeIndex(typeid(T));
+			auto iter = components.find(typeIndex);
+			if (iter != components.end()) {
+				return static_cast<T*>(iter->second.get());
 			}
 
 			return nullptr;
+		}
+
+		template <typename T> bool HasComponent() const {
+			return components.find(std::type_index(typeid(T))) != components.end();
 		}
 	};
 } // namespace Sirius
